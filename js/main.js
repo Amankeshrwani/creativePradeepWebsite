f92af47ed8c087f28e4b9c6dd77f99fc1b175a11
Aman <amankesharwani37561@gmail.com>
2023-03-04 16:41:07 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+
+let navbartop = document.getElementById("navbartop");
+let navul = document.getElementById("navul");
+let navli = document.getElementById("navbartop");
+
+function navbar(e){
+    console.log("hello world");
+    navbartop.classList.toggle("navbartop");
+    navul.classList.toggle("navul1");
+    navul.classList.toggle("disnone");
+}
